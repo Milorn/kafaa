347693a82dfbc318c5dcac9fdad2a4d2406c3b46
Karim Ouadhi <karim.ouadhi.dz@outlook.com>
2024-07-07 18:12:46 +0100

    Home page

--- resources/views/components/ui/header.blade.php
+++ resources/views/components/ui/header.blade.php
@@ -1,4 +1,4 @@
-<header class="bg-white py-6 px-5 border-b-2 border-b-primary">
+<header class="bg-white py-6 px-5 border-b-2 border-b-primary sticky top-0 z-50">
     <nav class="flex justify-between items-center mx-auto max-w-screen-xl">
         <div class="flex gap-6">
             <img class="h-16 border-r border-primary pr-6 py-2" src="{{asset('images/cerefe_logo.png')}}" alt="Logo Cerefe">
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,16 +13,117 @@
                             the industry's standard dummy text ever since the 1500s, when an unknown printer took a
                             galley of type and scrambled it to make a type.
                         </p>
-                        <button class="btn btn-icon bg-white text-primary">En savoir plus <x-heroicon-o-arrow-right class="size-5 text-primary stroke-2"/> </button>
+                        <button class="btn btn-icon bg-white text-primary">En savoir plus <x-heroicon-o-arrow-right
+                                class="size-5 text-primary stroke-2" /> </button>
                     </div>
                 </div>
             </div>
-            <img class="h-[946px] w-full" src="{{ asset('images/hero.png') }}">
+
+            <img class="h-[946px] w-full" src="{{ asset('images/home/hero.png') }}">
         </div>
 
+        <div
+            class="bg-[#45AC4C] max-w-screen-xl mx-auto py-8 px-4 rounded-xl shadow-[0px_9px_11px_0px_#149455] mt-[-220px] sticky top-[121px] z-50">
+            <div class="flex flex-col gap-3 text-center">
+                <h1 class="text-white font-bold text-3xl">Trouvez un installateur labellisé</h1>
+                <p class="text-xl text-white opacity-60">
+                    Kafaa is simply dummy text of the printing and typesetting
+                    industry. <br> Lorem Ipsum has been
+                </p>
+                <div class="grid grid-cols-3 gap-6">
+                    <div class="bg-white flex justify-center items-center gap-2 py-5 px-8 rounded-md">
+                        <span class="text-trivial">Type d'installation</span>
+                        <x-heroicon-o-chevron-down class="size-5 text-trivial" />
+                    </div>
 
-        <div class="bg-primary w-full h-screen">
+                    <div class="bg-white py-5 px-8 rounded-md">
+                        <span class="text-trivial">Veuillez saisir adresse, ville, code postal</span>
+                    </div>
+                    <button class="btn-secondary text-xl">Rechercher</button>
+                </div>
+            </div>
+        </div>
 
+        <div class="bg-primary mt-[-180px] pt-56 pb-20">
+            <div class="max-w-screen-xl mx-auto flex flex-col items-center gap-5">
+                <h1 class="font-bold text-6xl text-white">Les labels</h1>
+                <p class="text-center text-white text-xl opacity-60 max-w-screen-lg">
+                    It is a long established fact that a reader will be distracted by the readable content of a page
+                    when
+                    looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal
+                    distribution
+                    of letters, as opposed to using 'Content here, content here', making it look like readable English.
+                </p>
+                <div class="max-w-2xl grid grid-cols-2 gap-8 mt-10">
+                    <div class="flex flex-col items-center">
+                        <div class="bg-white py-10 px-16 flex justify-center rounded-lg">
+                            <img class="h-52 object-contain" src="{{ asset('images/label_pv_logo.png') }}"
+                                alt="Logo Label EPE">
+                        </div>
+                        <h2 class="text-center text-white font-bold text-3xl mt-10 mb-2">Kafa'a PV</h2>
+                        <p class="text-center text-lg text-white">
+                            It is a long established fact that a reader will be distracted by the readable content of a
+                            page
+                            when looking at its layout.
+                        </p>
+                        <button class="btn-secondary mt-5 text-xl">En savoir plus</button>
+                    </div>
+
+                    <div class="flex flex-col items-center">
+                        <div class="bg-white py-10 px-20 flex justify-center rounded-lg">
+                            <img class="h-52" src="{{ asset('images/label_epe_logo.png') }}" alt="Logo Label EPE">
+                        </div>
+                        <h2 class="text-center text-white font-bold text-3xl mt-10 mb-2">Kafa'a Inara</h2>
+                        <p class="text-center text-lg text-white">
+                            It is a long established fact that a reader will be distracted by the readable content of a
+                            page
+                            when looking at its layout.
+                        </p>
+                        <button class="btn-secondary mt-5 text-xl">En savoir plus</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="bg-[#066938] bg-opacity-20 py-20">
+            <div class="max-w-screen-xl mx-auto flex flex-col items-center gap-5 ">
+                <h1 class="font-bold text-6xl text-primary">Les avantages d'un label</h1>
+                <p class="text-center text-[#2E2E2E] text-xl max-w-screen-lg">
+                    It is a long established fact that a reader will be distracted by the readable content of a page
+                    when
+                    looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal
+                    distribution
+                    of letters, as opposed to using 'Content here, content here', making it look like readable English.
+                </p>
+
+                <div class="max-w-screen-lg grid grid-cols-3 gap-10 mt-10">
+                    <div class="flex flex-col gap-8 justify-center items-center">
+                        <img class="h-52 object-contain" src="{{ asset('images/home/solar_panel.png') }}">
+                        <h3 class="text-4xl text-primary font-bold">Avantage 1</h3>
+                        <p class="text-center w-2/3">
+                            It is a long established fact that a reader will be distracted by the readable content of a
+                            when looking at its layout.
+                        </p>
+                    </div>
+                    <div class="flex flex-col gap-8 justify-center items-center">
+                        <img class="h-52 object-contain" src="{{ asset('images/home/sun.png') }}">
+                        <h3 class="text-4xl text-primary font-bold">Avantage 2</h3>
+                        <p class="text-center w-2/3">
+                            It is a long established fact that a reader will be distracted by the readable content of a
+                            when looking at its layout.
+                        </p>
+                    </div>
+                    <div class="flex flex-col gap-8 justify-center items-center">
+                        <img class="h-52 object-contain" src="{{ asset('images/home/bulb.png') }}">
+                        <h3 class="text-4xl text-primary font-bold">Avantage 3</h3>
+                        <p class="text-center w-2/3">
+                            It is a long established fact that a reader will be distracted by the readable content of a
+                            when looking at its layout.
+                        </p>
+                    </div>
+                </div>
+                <button class="btn-primary mt-5 text-4xl px-24 py-5">Demander une labélisation</button>
+            </div>
         </div>
 
 
