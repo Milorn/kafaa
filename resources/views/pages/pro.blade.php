c5854276aefd6ec8b1543a6df6895381e0ca2612
Karim Ouadhi <karim.ouadhi.dz@outlook.com>
2024-08-25 16:55:21 +0100

    Temoignage

--- resources/views/pages/pro.blade.php
+++ resources/views/pages/pro.blade.php
@@ -30,11 +30,23 @@
                     <img class="h-52" src="{{ asset('images/label_epe_logo.png') }}" alt="Logo Label EPE">
                     <button class="btn-primary mt-5 text-xl px-28">S'inscrire</button>
                 </div>
-            </div>    
+            </div>
         </div>
 
-        <div class="bg-primary p-24 flex justify-center items-center">
-            <h1 class="text-white font-extrabold text-5xl">Témoignages</h1>
+        <div class="bg-primary p-24 flex flex-col justify-center items-center">
+            <h1 class="text-white font-extrabold text-5xl mb-9">Témoignages</h1>
+            <div class="flex justify-center gap-16  max-w-screen-sm">
+                <div class="flex flex-col justify-center items-center gap-5 w-full">
+                    <img class="rounded-full" src="{{ asset('images/placeholder.webp') }}"
+                        alt="Avatar placeholder">
+                    <p class="text-white text-lg font-bold">Nom Prénom</p>
+                </div>
+                <p class="text-white text-lg text-justify">
+                    The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.
+                    Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in
+                    their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+                </p>
+            </div>
         </div>
 
 
