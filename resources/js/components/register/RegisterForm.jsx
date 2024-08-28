e27c9f729bb26e728953327b1f45b647cd845e4d
Karim Ouadhi <karim.ouadhi.dz@outlook.com>
2024-08-28 15:32:07 +0100

    Progress on forms

--- resources/js/components/register/RegisterForm.jsx
+++ resources/js/components/register/RegisterForm.jsx
@@ -6,8 +6,8 @@ import RegisterProvider from "./RegisterProvider";
 
 export default function RegisterForm() {
     const [type, setType] = useState('expert');
-    const [label, setLabel] = useState('');
-    const [submitting, setSubmitting] = useState(false); 
+    const [label, setLabel] = useState('epe');
+    const [submitting, setSubmitting] = useState(false);
 
     const submit = (e) => {
         e.preventDefault();
@@ -34,31 +34,51 @@ export default function RegisterForm() {
                         </div>
                     </div>
 
-                    <div className="fieldset">
-                        <label htmlFor="label">Choisissez un label :</label>
-                        <div className="relative">
-                            <select id="label" className="w-full shadow-bottom" onChange={(e) => setLabel(e.target.value)} defaultValue={label}>
-                                <option value="" disabled>Please select</option>
-                                <option value="epe">EPE</option>
-                                <option value="pv">PV</option>
-                            </select>
-                            <svg className="pointer-events-none absolute top-0 right-0 mt-3 mr-3" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                                <polyline points="6 9 12 15 18 9"></polyline>
-                            </svg>
+                    {
+                        type == "expert" &&
+                        <div className="fieldset">
+                            <label htmlFor="label">Choisissez un label :</label>
+                            <div className="relative">
+                                <select id="label" className="w-full shadow-bottom" onChange={(e) => setLabel(e.target.value)} defaultValue={label}>
+                                    <option value="" disabled>Please select</option>
+                                    <option value="epe">EPE</option>
+                                    <option value="pv">PV</option>
+                                </select>
+                                <svg className="pointer-events-none absolute top-0 right-0 mt-3 mr-3" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                                    <polyline points="6 9 12 15 18 9"></polyline>
+                                </svg>
+                            </div>
+                        </div>
+                    }
+                </div>
+                {
+                    type &&
+                    <div className="mt-10">
+                        {
+                            type == 'company' ? <RegisterCompany />
+                                : type == 'provider' ? <RegisterProvider />
+                                    : type == 'expert' && label && <RegisterExpert label={label} />
+                        }
+                    </div>
+                }
+
+                <hr className="my-7" />
+
+                <div className="flex flex-col gap-y-5">
+                    <div className="flex items-center gap-3">
+                        <input id="charter" type="checkbox" className="size-4" />
+                        <label htmlFor="charter">Je m'engage au respect de la charte du label</label>
+                    </div>
+                    <div className="flex gap-3">
+                        <input id="conditions" type="checkbox" className="size-4" />
+                        <div className="flex flex-col gap-2">
+                            <label htmlFor="conditions">Déclaration d'engagement au respect de la charte du label</label>
+                            <p className="text-trivial text-sm">
+                                Je déclare sur l'honneur l'exactitude des informations fournies dans ce formulaire. Je suis conscient que le non-respect des engagements pris dans ce formulaire peut entraîner la suspension ou la révocation de mon adhésion au label solaire PV.
+                            </p>
                         </div>
                     </div>
                 </div>
-                {
-                    type && label && (
-                        <div className="mt-10">
-                            {
-                                type == 'expert' ? <RegisterExpert label={label}/>
-                                    : type == 'company' ? <RegisterCompany label={label}/>
-                                        : <RegisterProvider label={label}/>
-                            }
-                        </div>
-                    )
-                }
             </form>
         </>
     )
